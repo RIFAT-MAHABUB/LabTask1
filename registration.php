4872af06b802a88436aad967fec062469c08ca31
RIFAT-MAHABUB <78336614+RIFAT-MAHABUB@users.noreply.github.com>
2021-02-04 19:43:18 +0600

    Initial commit

---
+++ registration.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>REGISTRATION</title>
+
+</head>
+<body>
+    <h1 align="center">REGISTRATION</h1>
+</body>
+<from align="center">
+Name:
+<input type="text" id="fname" name="fname"><br><br>
+Email:
+<input type="text" id="e" name="email"><br><br>
+User Name:
+<input type="text" id="uname" name="username"><br><br>
+Password:
+<input type="password" id="e" name="email"><br><br>
+Confirm Password:
+<input type="password" id="e" name="email"><br><br>
+Gender:<br>
+<input type="radio" id="gender" name="male" value="male"><br><br>
+<input type="radio" id="gender" name="female"value="female"><br><br>
+<input type="radio" id="gender" name="other"value="other"><br><br>
+
+Date of birth:
+<input type="date"><br><br>
+<input type="submit" value="submit">
+<input type="reset" value="reset">
+
+
+
+
+
+
+</from>
+
+</html>
